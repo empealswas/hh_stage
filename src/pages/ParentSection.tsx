3112f69dae2f0029a27fdf5e9a2c0699f895f9a9
HlibDavdydov <Hlib@healthcareinsafehands.com>
2021-10-04 17:47:15 +0300

    parent view setup

--- src/pages/ParentSection.tsx
+++ src/pages/ParentSection.tsx
@@ -1,48 +1,10 @@
-import {filter} from 'lodash';
-import {Icon} from '@iconify/react';
-import {sentenceCase} from 'change-case';
-import {useState} from 'react';
-import plusFill from '@iconify/icons-eva/plus-fill';
-import {Link as RouterLink} from 'react-router-dom';
 // material
-import {
-    Card,
-    Table,
-    Stack,
-    Avatar,
-    Button,
-    Checkbox,
-    TableRow,
-    TableBody,
-    TableCell,
-    Container,
-    Typography,
-    TableContainer,
-    TablePagination
-} from '@material-ui/core';
+import {Card, Container, CardMedia, CardHeader} from '@material-ui/core';
 // components
 import Page from '../components/Page';
-import Label from '../components/Label';
-import Scrollbar from '../components/Scrollbar';
-import SearchNotFound from '../components/SearchNotFound';
-import {UserListHead, UserListToolbar, UserMoreMenu} from '../components/_dashboard/user';
+import {Avatar, Button, CardActions, CardContent, Stack, Typography} from "@mui/material";
+import MyTabs from "../components/parent/tabs";
 //
-import USERLIST from '../_mocks_/user';
-import ProgressButton from "../components/Buttons/ProgressButton";
-
-
-// ----------------------------------------------------------------------
-
-const TABLE_HEAD = [
-    {id: 'name', label: 'Name', alignRight: false},
-    {id: 'company', label: 'Company', alignRight: false},
-    {id: 'role', label: 'Role', alignRight: false},
-    {id: 'isVerified', label: 'Verified', alignRight: false},
-    {id: 'status', label: 'Status', alignRight: false},
-    {id: ''}
-];
-
-// ----------------------------------------------------------------------
 
 
 const ParentSection = () => {
@@ -50,15 +12,47 @@ const ParentSection = () => {
     return (
         // @ts-ignore
         <Page>
-            <Container>
-                <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-                    <Typography variant="h4" gutterBottom>
-                        Children
-                    </Typography>
-                </Stack>
-                <Typography variant={'h3'} color={'error'}>
-                    This section is under construction
-                </Typography>
+            <Container maxWidth={false}>
+                <Card>
+                    <CardMedia
+                        component="img"
+                        image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wallpapers13.com%2Fwp-content%2Fuploads%2F2016%2F01%2FRarotonga-cook-isls-desktop-background-498319.jpg&f=1&nofb=1"
+                        alt="Paella dish"
+                        style={{
+                            width: '100%',
+                            height: 'calc(80px + 10vw)',
+                            textAlign: 'center'
+                        }}
+                    />
+                    <Container maxWidth={false}>
+                        <Stack direction={{xs: 'column', sm: 'row'}} justifyContent={{xs: 'center', sm: 'space-between'}} alignItems={'center'}>
+                            <Stack direction={{xs: 'column', sm: 'row'}} spacing={{xs: 0, sm: 2}}>
+                                <Avatar sx={{
+                                    width: 'calc(80px + 5vw)',
+                                    height: 'calc(80px + 5vw)',
+                                    border: '3px solid white',
+                                    margin: '-60px auto 0'
+                                }} src={'/static/mock-images/avatars/avatar_12.jpg'}/>
+                                <div>
+                                    <Typography variant={'h5'}>
+                                        Hlib Davydov
+                                    </Typography>
+                                    <Typography variant={'subtitle2'}>
+                                        Class 7A
+                                    </Typography>
+                                </div>
+                            </Stack>
+                            <Stack direction={'row'} height={40} spacing={2}>
+                                <Button variant={'outlined'} color={'secondary'}>Action 1</Button>
+                                <Button variant={'contained'} color={'secondary'}>Action 1</Button>
+                            </Stack>
+                        </Stack>
+                    </Container>
+                    <CardActions>
+                        <MyTabs/>
+                    </CardActions>
+
+                </Card>
             </Container>
         </Page>
     );
