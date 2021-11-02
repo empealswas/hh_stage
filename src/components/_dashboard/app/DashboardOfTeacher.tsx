81e62d3ef3c4d5411b1e91288a744280987cbade
tonylindsayqub <31003309+tonylindsay-h9@users.noreply.github.com>
2021-11-02 23:58:29 +0000

    Pass values into component using GarminQueryData model & create working URL

--- src/components/_dashboard/app/DashboardOfTeacher.tsx
+++ src/components/_dashboard/app/DashboardOfTeacher.tsx
@@ -1,21 +1,25 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {API, graphqlOperation} from "aws-amplify";
-import {UserContext} from "../../../App";
-import {Classroom, Lesson, Term} from '../../../API';
+import React, { useContext, useEffect, useState } from 'react';
+import { API, graphqlOperation } from "aws-amplify";
+import { UserContext } from "../../../App";
+import { Classroom, Lesson, Term } from '../../../API';
 import Grid from "@material-ui/core/Grid";
-import {Connect} from "aws-amplify-react";
-import {IConnectState} from "aws-amplify-react/lib/API/GraphQL/Connect";
+import { Connect } from "aws-amplify-react";
+import { IConnectState } from "aws-amplify-react/lib/API/GraphQL/Connect";
 import CardSkeleton from "../../skeletons/CardSkeleton";
 import Link from "@material-ui/core/Link";
-import {Link as RouterLink} from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import {Container, Stack} from "@material-ui/core";
-import ChooseLessonPlanSearchField, {LessonPlansSearchField} from "../../reports/ChooseSubjectSearchField";
+import { Container, Stack } from "@material-ui/core";
+import ChooseLessonPlanSearchField, { LessonPlansSearchField } from "../../reports/ChooseSubjectSearchField";
 import TermReport from "../../reports/TermReport";
 import SchoolHousesPage from "../../../pages/SchoolHousesPage";
+import ActivityLineChart from '../../reports/charts/ActivityLineChart';
+import HeatMapChart from '../../reports/charts/HeatMap';
+import DailiesMixedData from '../../reports/charts/GarminWearablesCharts/DailiesMixedData';
+import { GarminQueryData } from '../../../models/garminDataModels/garminQueryData';
 
 const query =/*GraphQL*/`query MyQuery($id: ID = "") {
     getTeacher(id: $id) {
@@ -60,11 +64,11 @@ const DashboardOfTeacher = () => {
     const [classrooms, setClassrooms] = useState<null | Classroom[]>(null);
     const [selectedTerm, setSelectedTerm] = useState<Term | null>(null);
     const [terms, setTerms] = useState<Term[] | null>(null);
-
+   
     useEffect(() => {
         console.log(user);
         const fetchLessons = async (): Promise<any> => {
-            const lessonsData: any = await API.graphql(graphqlOperation(query, {id: user?.email}));
+            const lessonsData: any = await API.graphql(graphqlOperation(query, { id: user?.email }));
             const terms: Term[] = lessonsData.data.getTeacher.classrooms.items
                 .filter((item: any) => !!item?.classroom?.yearGroup)
                 .flatMap((item: any) => item?.classroom?.yearGroup?.subjects?.items)
@@ -82,22 +86,32 @@ const DashboardOfTeacher = () => {
 
         };
     }, []);
-
+    var queryData = new GarminQueryData('2021-07-01', '2021-11-01', 'daily', 'user');
     return (
-        <>
+        <Container>
 
-            {/*<Grid item xs={12} md={12} lg={6}>*/}
-            {/*    <>*/}
-            {/*        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>*/}
-            {/*            <Typography variant="h4" gutterBottom>*/}
-            {/*                Reports*/}
-            {/*            </Typography>*/}
-            {/*            <LessonPlansSearchField terms={terms} setSelectedTerm={setSelectedTerm}/>*/}
-            {/*        </Stack>*/}
-            {/*        {selectedTerm && <TermReport term={selectedTerm}/>}*/}
-            {/*    </>*/}
-            {/*</Grid>*/}
-        </>);
+            <h1>Whatis in here then!!!!!!!!!!!!!!!!!</h1>
+          
+            <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}> 
+            <Grid item xs={12} sm={6} md={6} lg={6}>
+                    <DailiesMixedData {...queryData}/>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6} lg={6}>
+                    <HeatMapChart />
+                </Grid>
+                </Stack>
+            {/* <Grid item xs={12} md={12} lg={6}> */}
+            {/* <> */}
+            {/* <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}> */}
+            {/* <Typography variant="h4" gutterBottom> */}
+            {/* Reports for tony */}
+            {/* </Typography> */}
+            {/* <LessonPlansSearchField terms={terms} setSelectedTerm={setSelectedTerm}/> */}
+            {/* </Stack> */}
+            {/* {selectedTerm && <TermReport term={selectedTerm}/>} */}
+            {/* </> */}
+            {/* </Grid> */}
+        </Container>);
     // <Typography variant={'h5'}>
     //     Your Classrooms:
     //     {classrooms?.map(classroom => `${classroom.name} | `)}
@@ -110,74 +124,74 @@ const DashboardOfTeacher = () => {
     // {/*>*/}
     // {/*</iframe>*/}
 
-    {/*<Grid container*/
-    }
-    {/*      direction="row"*/
-    }
-    {/*      justifyContent="flex-start"*/
-    }
-    {/*      alignItems="flex-start" spacing={2}*/
-    }
-    {/*>*/
-    }
-    {/*    <Grid item xs={12} minHeight={200}>*/
-    }
-    {/*        <Grid container justifyContent="center" spacing={2}*/
-    }
-    {/*              style={{flexGrow: 1, display: 'flex', flexWrap: 'wrap'}}>*/
-    }
-    {/*            {lessons ?*/
-    }
-    {/*                lessons.map((lesson: Lesson, index) => (*/
-    }
-    {/*                    <Grid key={index} item xs={12} sm={6} md={3} maxWidth={300} minWidth={200}>*/
-    }
-    {/*                        <Link component={RouterLink} to={`../curricula/subjects/terms/lessons/${lesson.id}`}*/
-    }
-    {/*                              underline={'none'}>*/
-    }
-    {/*                            <Card style={{height: '100%'}}>*/
-    }
-    {/*                                <CardActionArea style={{height: '100%'}}>*/
-    }
-    {/*                                    <CardContent style={{textAlign: 'center'}}>*/
-    }
-    {/*                                        <Typography variant="h5" component="h2">*/
-    }
-    {/*                                            {lesson.title}*/
-    }
-    {/*                                        </Typography>*/
-    }
-    {/*                                    </CardContent>*/
-    }
-    {/*                                </CardActionArea>*/
-    }
-    {/*                            </Card>*/
-    }
-    {/*                        </Link>*/
-    }
-    {/*                    </Grid>*/
-    }
-    {/*                ))*/
-    }
-    {/*                :*/
-    }
-    {/*                [0, 1, 2, 3, 4, 5].map((value) => (*/
-    }
-    {/*                    <CardSkeleton key={value}/>*/
-    }
-    {/*                ))*/
-    }
-    {/*            }*/
-    }
-    {/*        </Grid>*/
-    }
-    {/*    </Grid>*/
-    }
-    {/*</Grid>*/
-    }
-// </Container>
-// )
+    // {/*<Grid container*/
+    // }
+    // {/*      direction="row"*/
+    // }
+    // {/*      justifyContent="flex-start"*/
+    // }
+    // {/*      alignItems="flex-start" spacing={2}*/
+    // }
+    // {/*>*/
+    // }
+    // {/*    <Grid item xs={12} minHeight={200}>*/
+    // }
+    // {/*        <Grid container justifyContent="center" spacing={2}*/
+    // }
+    // {/*              style={{flexGrow: 1, display: 'flex', flexWrap: 'wrap'}}>*/
+    // }
+    // {/*            {lessons ?*/
+    // }
+    // {/*                lessons.map((lesson: Lesson, index) => (*/
+    // }
+    // {/*                    <Grid key={index} item xs={12} sm={6} md={3} maxWidth={300} minWidth={200}>*/
+    // }
+    // {/*                        <Link component={RouterLink} to={`../curricula/subjects/terms/lessons/${lesson.id}`}*/
+    // }
+    // {/*                              underline={'none'}>*/
+    // }
+    // {/*                            <Card style={{height: '100%'}}>*/
+    // }
+    // {/*                                <CardActionArea style={{height: '100%'}}>*/
+    // }
+    // {/*                                    <CardContent style={{textAlign: 'center'}}>*/
+    // }
+    // {/*                                        <Typography variant="h5" component="h2">*/
+    // }
+    // {/*                                            {lesson.title}*/
+    // }
+    // {/*                                        </Typography>*/
+    // }
+    // {/*                                    </CardContent>*/
+    // }
+    // {/*                                </CardActionArea>*/
+    // }
+    // {/*                            </Card>*/
+    // }
+    // {/*                        </Link>*/
+    // }
+    // {/*                    </Grid>*/
+    // }
+    // {/*                ))*/
+    // }
+    // {/*                :*/
+    // }
+    // {/*                [0, 1, 2, 3, 4, 5].map((value) => (*/
+    // }
+    // {/*                    <CardSkeleton key={value}/>*/
+    // }
+    // {/*                ))*/
+    // }
+    // {/*            }*/
+    // }
+    // {/*        </Grid>*/
+    // }
+    // {/*    </Grid>*/
+    // }
+    // {/*</Grid>*/
+    // }
+    // // </Container>
+    // // )
 };
 
 export default DashboardOfTeacher;
