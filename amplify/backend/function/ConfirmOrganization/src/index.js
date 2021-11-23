f533ff78031a7ad42c067d852740501b187c36b0
s19208 <s19208@pjwstk.edu.pl>
2021-11-23 23:12:58 +0100

    backend bug fixed

--- amplify/backend/function/ConfirmOrganization/src/index.js
+++ amplify/backend/function/ConfirmOrganization/src/index.js
@@ -8,15 +8,14 @@ Amplify Params - DO NOT EDIT */
 var aws = require('aws-sdk');
 const cognito = new aws.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
 exports.handler = (event, context, callback) => {
-    const body = event.body
-    console.log(body)
+    const body = event.body;
     var confirmParams = {
         UserPoolId: process.env.AUTH_HEALTHYHABITSV24AEA1E5F_USERPOOLID, /* required */
         Username: body.email /* required */
     };
     cognito.adminConfirmSignUp(confirmParams, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
-        console.log(data);
+        // Return to Amazon Cognito
         callback(null, data);
 
         // return;
