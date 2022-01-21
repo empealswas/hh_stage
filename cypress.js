be4963be4377e4fac5779efea36c1674943e5ef8
HlibDavdydov <Hlib@healthcareinsafehands.com>
2022-01-21 17:24:43 +0100

    update

--- cypress.js
+++
@@ -1,9 +0,0 @@
-const marge = require('mochawesome-report-generator')
-
-const { merge } = require('mochawesome-merge')
-
-const reports = { reportDir: 'mochawesome-report' }
-
-const mergedReport = { reportDir: 'cypress/report/mochawesome-report' }
-
-merge(reports).then((report) => marge.create(report, mergedReport))
--- src/types.ts
+++
@@ -1,5 +0,0 @@
-export type ActivityCardProps = {
-    linkTo: string,
-    imagePath: string | undefined | null,
-    title: string,
-}
