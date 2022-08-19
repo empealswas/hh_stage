200082afe5a4871c80d42ad6b6fb2a725d5d9b30
marksaunders341 <103631116+marksaunders341@users.noreply.github.com>
2022-08-19 16:03:55 +0100

    User dashboard average data added

--- src/pages/dashboard/parent/child/profile_tabs/ChildActivitiesSummary.tsx
+++ src/pages/dashboard/parent/child/profile_tabs/ChildActivitiesSummary.tsx
@@ -4,21 +4,30 @@ import {Box, Grid} from "@mui/material";
 import {User} from "../../../../../API";
 //import {SleepData, TerraData} from "../../../../../models/terraDataModels/TerraData";
 import {PupilActivityRequest} from "../../../../../apiFunctions/DTO/PupilActivityRequest";
-import {getDailySteps, getDailySleepSeconds} from "../../../../../apiFunctions/apiFunctions";
 import StepsChart from "./StepsChart";
 import PupilActivitiesChart from "./PupilActivitiesChart";
 import SleepChart from "./SleepChart";
 import ConnectToWearableDeviceButton from "../wearable/ConnectToWearableDeviceButton";
 import {useTheme} from "@mui/material/styles";
 import ActivityWidgets from "./activity/ActivityWidgets";
+import {getWearablesData, TerraWearables} from "../../../../../apiFunctions/apiFunctions";
 
 export const StepsDataContext = React.createContext<any | null>(null);
 export const SleepDataContext = React.createContext<any | null>(null);
+export const ActivityDataContext = React.createContext<any | null>(null);
+export const AverageStepsContext = React.createContext<any | null>(null);
+export const AverageSleepContext = React.createContext<any | null>(null);
+export const AverageActivityContext = React.createContext<any | null>(null);
 
 const ChildActivitiesSummary = (props: {user: User}) => {
 
     const [stepsData, setStepsData] = useState<any | null>(null);
     const [sleepData, setSleepData] = useState<any | null>(null);
+    const [activityData, setActivityData] = useState<any | null>(null);
+    const [averageSteps, setAverageSteps] = useState<any | null>(null);
+    const [averageSleep, setAverageSleep] = useState<any | null>(null);
+    const [averageActivity, setAverageActivity] = useState<any | null>(null);
+
     const theme = useTheme();
 
     const getStepsData = async () => {
@@ -27,9 +36,17 @@ const ChildActivitiesSummary = (props: {user: User}) => {
         if (terraId == null) {
             return;
         }
-        let startDate = subDays(new Date(), 6);
-        let endDate = new Date();
-        const result = await getDailySteps(terraId, startDate, endDate);
+        let requestBody: TerraWearables = {
+            "idList": [terraId],
+            "grouping": "user",
+            "category": "daily",
+            "subtype": "steps",
+            "period": "day",
+            "startDate": format(subDays(new Date(), 6), "yyyy-MM-dd"),
+            "endDate": format(new Date(), "yyyy-MM-dd"),
+            "returnType": "total"
+        };
+        const result = await getWearablesData(requestBody);
         setStepsData(result);
     }
 
@@ -39,9 +56,17 @@ const ChildActivitiesSummary = (props: {user: User}) => {
         if (terraId == null) {
             return;
         }
-        let startDate = subDays(new Date(), 6);
-        let endDate = new Date();
-        const result = await getDailySleepSeconds(terraId, startDate, endDate);
+        let requestBody: TerraWearables = {
+            "idList": [terraId],
+            "grouping": "user",
+            "category": "sleep",
+            "subtype": "durationTotal",
+            "period": "day",
+            "startDate": format(subDays(new Date(), 6), "yyyy-MM-dd"),
+            "endDate": format(new Date(), "yyyy-MM-dd"),
+            "returnType": "total"
+        };
+        const result = await getWearablesData(requestBody);
         result?.data?.sort((a: any, b: any) => {
             let aMillis = new Date(a.date).getTime();
             let bMillis = new Date(b.date).getTime();
@@ -50,29 +75,121 @@ const ChildActivitiesSummary = (props: {user: User}) => {
         setSleepData(result);
     }
 
+    const getActivityData = async () => {
+        setActivityData(null);
+        let terraId = props.user.terraId;
+        if (terraId == null) {
+            return;
+        }
+        let requestBody: TerraWearables = {
+            "idList": [terraId],
+            "grouping": "user",
+            "category": "activity",
+            "subtype": "duration",
+            "period": "day",
+            "startDate": format(subDays(new Date(), 6), "yyyy-MM-dd"),
+            "endDate": format(new Date(), "yyyy-MM-dd"),
+            "returnType": "total"
+        };
+        const result = await getWearablesData(requestBody);
+        setActivityData(result);
+    }
+
+    const getAverageSteps = async () => {
+        setAverageSteps(null);
+        let terraId = props.user.terraId;
+        if (terraId == null) {
+            return;
+        }
+        let requestBody: TerraWearables = {
+            "idList": [terraId],
+            "grouping": "user",
+            "category": "daily",
+            "subtype": "steps",
+            "period": "millennium",
+            "startDate": format(subDays(new Date(), 6), "yyyy-MM-dd"),
+            "endDate": format(new Date(), "yyyy-MM-dd"),
+            "returnType": "average"
+        };
+        const result = await getWearablesData(requestBody);
+        setAverageSteps(result);
+    }
+
+    const getAverageSleep = async () => {
+        setAverageSleep(null);
+        let terraId = props.user.terraId;
+        if (terraId == null) {
+            return;
+        }
+        let requestBody: TerraWearables = {
+            "idList": [terraId],
+            "grouping": "user",
+            "category": "sleep",
+            "subtype": "durationTotal",
+            "period": "millennium",
+            "startDate": format(subDays(new Date(), 6), "yyyy-MM-dd"),
+            "endDate": format(new Date(), "yyyy-MM-dd"),
+            "returnType": "average"
+        };
+        const result = await getWearablesData(requestBody);
+        setAverageSleep(result);
+    }
+
+    const getAverageActivity = async () => {
+        setAverageActivity(null);
+        let terraId = props.user.terraId;
+        if (terraId == null) {
+            return;
+        }
+        let requestBody: TerraWearables = {
+            "idList": [terraId],
+            "grouping": "user",
+            "category": "activity",
+            "subtype": "duration",
+            "period": "millennium",
+            "startDate": format(subDays(new Date(), 6), "yyyy-MM-dd"),
+            "endDate": format(new Date(), "yyyy-MM-dd"),
+            "returnType": "average"
+        };
+        const result = await getWearablesData(requestBody);
+        setAverageActivity(result);
+    }
+
     useEffect(() => {
         getStepsData();
         getSleepData()
+        getActivityData();
+        getAverageSteps();
+        getAverageSleep();
+        getAverageActivity();
         return () => {};
     }, [props.user]);
 
     return (
         <StepsDataContext.Provider value={stepsData}>
             <SleepDataContext.Provider value={sleepData}>
-                <ConnectToWearableDeviceButton user={props.user}/>
-                <Box height={5}/>
-                <Grid container spacing={3}>
-                    <ActivityWidgets/>
-                    <Grid item xs={12} md={12} lg={12}>
-                        <StepsChart userId={props.user.id}/>
-                    </Grid>
-                    <Grid item xs={12} md={12} lg={12}>
-                        <SleepChart/>
-                    </Grid>
-                    <Grid item xs={12} md={12} lg={12}>
-                        <PupilActivitiesChart/>
-                    </Grid>
-                </Grid>
+                <ActivityDataContext.Provider value={activityData}>
+                    <AverageStepsContext.Provider value={averageSteps}>
+                        <AverageSleepContext.Provider value={averageSleep}>
+                            <AverageActivityContext.Provider value={averageActivity}>
+                                <ConnectToWearableDeviceButton user={props.user}/>
+                                <Box height={5}/>
+                                <Grid container spacing={3}>
+                                    <ActivityWidgets/>
+                                    <Grid item xs={12} md={12} lg={12}>
+                                        <StepsChart userId={props.user.id}/>
+                                    </Grid>
+                                    <Grid item xs={12} md={12} lg={12}>
+                                        <SleepChart/>
+                                    </Grid>
+                                    <Grid item xs={12} md={12} lg={12}>
+                                        <PupilActivitiesChart/>
+                                    </Grid>
+                                </Grid>
+                            </AverageActivityContext.Provider>
+                        </AverageSleepContext.Provider>
+                    </AverageStepsContext.Provider>
+                </ActivityDataContext.Provider>
             </SleepDataContext.Provider>
         </StepsDataContext.Provider>
     );
--- src/pages/dashboard/parent/child/profile_tabs/activity/ActivityWidgets.tsx
+++ src/pages/dashboard/parent/child/profile_tabs/activity/ActivityWidgets.tsx
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Grid} from "@mui/material";
-import {StepsDataContext, SleepDataContext} from "../ChildActivitiesSummary";
+import {Grid, Card, CardContent, Typography} from "@mui/material";
+import {StepsDataContext, SleepDataContext, ActivityDataContext, AverageStepsContext, AverageSleepContext, AverageActivityContext} from "../ChildActivitiesSummary";
 import ActivityWidgetSummary from "./ActivityWidgetSummary";
 import CardSkeleton from "../../../../../../components/skeleton/CardSkeleton";
 import {useTheme} from "@mui/material/styles";
@@ -10,38 +10,21 @@ import {format, subDays} from "date-fns";
 import axios from "axios";
 import {useParams} from "react-router-dom";
 import useAuth from "../../../../../../hooks/useAuth";
-import {getDailyActivitySeconds} from "../../../../../../apiFunctions/apiFunctions";
 import sl from 'date-fns/locale/sl';
-
-const userQuery = `query MyQuery($id: ID = "") {
-  getUser(id: $id) {
-    terraId
-  }
-}`
 
 const ActivityWidgets = () => {
 
     const stepsData = useContext(StepsDataContext);
     const sleepData = useContext(SleepDataContext);
+    const activityData = useContext(ActivityDataContext);
+    const averageSteps = useContext(AverageStepsContext);
+    const averageSleep = useContext(AverageSleepContext);
+    const averageActivity = useContext(AverageActivityContext);
+
     const {user} = useAuth();
     const theme = useTheme();
 
-    const [activityData, setActivityData] = useState<any | null>(null);
-
     useEffect(() => {
-        const getActivityData = async () => {
-            setActivityData(null);
-            let result: any = await API.graphql(graphqlOperation(userQuery, {id: user?.email}));
-            let terraId = result.data.getUser.terraId;
-            if (terraId == null) {
-                return;
-            }
-            let startDate = subDays(new Date(), 6);
-            let endDate = new Date();
-            let theActivityData = await getDailyActivitySeconds(terraId, startDate, endDate);
-            setActivityData(theActivityData);
-        }
-        getActivityData();
         return () => {};
     }, []);
 
@@ -55,7 +38,7 @@ const ActivityWidgets = () => {
                                            chartColor={theme.palette.chart.green[0]}
                                            chartData={stepsData.data.map((item: any) => item.value)}/>
                     :
-                    <CardSkeleton/>
+                    <CardSkeleton height={'160px'}/>
                 }
             </Grid>
             <Grid item xs={12} md={4}>
@@ -66,7 +49,7 @@ const ActivityWidgets = () => {
                                            chartColor={theme.palette.chart.blue[0]}
                                            chartData={sleepData.data.map((item: any) => item.value / 60 / 60)}/>
                     :
-                    <CardSkeleton/>
+                    <CardSkeleton height={'160px'}/>
                 }
             </Grid>
             <Grid item xs={12} md={4}>
@@ -77,7 +60,43 @@ const ActivityWidgets = () => {
                                            chartColor={theme.palette.chart.green[0]}
                                            chartData={activityData.data.map((item: any) => item.value / 60)}/>
                     :
-                    <CardSkeleton/>
+                    <CardSkeleton height={'160px'}/>
+                }
+            </Grid>
+            <Grid item xs={12} md={4}>
+                {averageSteps ?
+                    <Card style={{backgroundColor:'#bb77ff', border:'4px solid black'}}>
+                        <CardContent>
+                            <Typography variant={'h5'} textAlign={'center'}>Average Steps</Typography>
+                            <Typography variant={'h3'} textAlign={'center'}>{Math.floor(averageSteps.data[0]?.value ?? 0).toLocaleString()}</Typography>
+                        </CardContent>
+                    </Card>
+                    :
+                    <CardSkeleton height={'135px'}/>
+                }
+            </Grid>
+            <Grid item xs={12} md={4}>
+                {averageSleep ?
+                    <Card style={{backgroundColor:'#77dd77', border:'4px solid black'}}>
+                        <CardContent>
+                            <Typography variant={'h5'} textAlign={'center'}>Average Sleep</Typography>
+                            <Typography variant={'h3'} textAlign={'center'}>{((averageSleep.data[0]?.value ?? 0) / 60 / 60).toFixed(1) + " Hours"}</Typography>
+                        </CardContent>
+                    </Card>
+                    :
+                    <CardSkeleton height={'135px'}/>
+                }
+            </Grid>
+            <Grid item xs={12} md={4}>
+                {averageActivity ?
+                    <Card style={{backgroundColor:'#ffaaff', border:'4px solid black'}}>
+                        <CardContent>
+                            <Typography variant={'h5'} textAlign={'center'}>Average Active Time</Typography>
+                            <Typography variant={'h3'} textAlign={'center'}>{Math.floor((averageActivity.data[0]?.value ?? 0) / 60) + " mins"}</Typography>
+                        </CardContent>
+                    </Card>
+                    :
+                    <CardSkeleton height={'135px'}/>
                 }
             </Grid>
         </>
