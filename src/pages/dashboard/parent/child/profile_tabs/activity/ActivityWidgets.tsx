abb330700e767069ef78f46651ac41e29e585a8d
marksaunders341 <103631116+marksaunders341@users.noreply.github.com>
2022-10-25 11:20:24 +0100

    User dashboard intensity metrics added

--- src/pages/dashboard/parent/child/profile_tabs/activity/ActivityWidgets.tsx
+++ src/pages/dashboard/parent/child/profile_tabs/activity/ActivityWidgets.tsx
@@ -1,6 +1,6 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {Grid, Card, CardContent, Typography} from "@mui/material";
-import {StepsDataContext, SleepDataContext, ActivityDataContext, AverageStepsContext, AverageSleepContext, AverageActivityContext} from "../ChildActivitiesSummary";
+import {StepsDataContext, SleepDataContext, ActivityDataContext, LowIntensityDataContext, ModerateIntensityDataContext, VigorousIntensityDataContext, AverageStepsContext, AverageSleepContext, AverageActivityContext} from "../ChildActivitiesSummary";
 import ActivityWidgetSummary from "./ActivityWidgetSummary";
 import CardSkeleton from "../../../../../../components/skeleton/CardSkeleton";
 import {useTheme} from "@mui/material/styles";
@@ -17,6 +17,9 @@ const ActivityWidgets = () => {
     const stepsData = useContext(StepsDataContext);
     const sleepData = useContext(SleepDataContext);
     const activityData = useContext(ActivityDataContext);
+    const lowIntensityData = useContext(LowIntensityDataContext);
+    const moderateIntensityData = useContext(ModerateIntensityDataContext);
+    const vigorousIntensityData = useContext(VigorousIntensityDataContext);
     const averageSteps = useContext(AverageStepsContext);
     const averageSleep = useContext(AverageSleepContext);
     const averageActivity = useContext(AverageActivityContext);
@@ -59,6 +62,39 @@ const ActivityWidgets = () => {
                                            percent={(((activityData?.data[activityData?.data?.length - 1]?.value ?? 0) - (activityData?.data[activityData?.data?.length - 2]?.value ?? 0))) / (activityData?.data[activityData?.data?.length - 2]?.value ?? 1) * 100}
                                            chartColor={theme.palette.chart.green[0]}
                                            chartData={activityData.data.map((item: any) => item.value / 60)}/>
+                    :
+                    <CardSkeleton height={'160px'}/>
+                }
+            </Grid>
+            <Grid item xs={12} md={4}>
+                {lowIntensityData ?
+                    <ActivityWidgetSummary title={'Todays\' Low Intensity'}
+                                           total={Math.floor((lowIntensityData?.data[lowIntensityData?.data?.length - 1]?.value ?? 0) / 60) + " mins"}
+                                           percent={(((lowIntensityData?.data[lowIntensityData?.data?.length - 1]?.value ?? 0) - (lowIntensityData?.data[lowIntensityData?.data?.length - 2]?.value ?? 0))) / (lowIntensityData?.data[lowIntensityData?.data?.length - 2]?.value ?? 1) * 100}
+                                           chartColor={theme.palette.chart.green[0]}
+                                           chartData={lowIntensityData.data.map((item: any) => item.value / 60)}/>
+                    :
+                    <CardSkeleton height={'160px'}/>
+                }
+            </Grid>
+            <Grid item xs={12} md={4}>
+                {moderateIntensityData ?
+                    <ActivityWidgetSummary title={'Todays\' Moderate Intensity'}
+                                           total={Math.floor((moderateIntensityData?.data[moderateIntensityData?.data?.length - 1]?.value ?? 0) / 60) + " mins"}
+                                           percent={(((moderateIntensityData?.data[moderateIntensityData?.data?.length - 1]?.value ?? 0) - (moderateIntensityData?.data[moderateIntensityData?.data?.length - 2]?.value ?? 0))) / (moderateIntensityData?.data[moderateIntensityData?.data?.length - 2]?.value ?? 1) * 100}
+                                           chartColor={theme.palette.chart.green[0]}
+                                           chartData={moderateIntensityData.data.map((item: any) => item.value / 60)}/>
+                    :
+                    <CardSkeleton height={'160px'}/>
+                }
+            </Grid>
+            <Grid item xs={12} md={4}>
+                {vigorousIntensityData ?
+                    <ActivityWidgetSummary title={'Todays\' Vigorous Intensity'}
+                                           total={Math.floor((vigorousIntensityData?.data[vigorousIntensityData?.data?.length - 1]?.value ?? 0) / 60) + " mins"}
+                                           percent={(((vigorousIntensityData?.data[vigorousIntensityData?.data?.length - 1]?.value ?? 0) - (vigorousIntensityData?.data[vigorousIntensityData?.data?.length - 2]?.value ?? 0))) / (vigorousIntensityData?.data[vigorousIntensityData?.data?.length - 2]?.value ?? 1) * 100}
+                                           chartColor={theme.palette.chart.green[0]}
+                                           chartData={vigorousIntensityData.data.map((item: any) => item.value / 60)}/>
                     :
                     <CardSkeleton height={'160px'}/>
                 }
