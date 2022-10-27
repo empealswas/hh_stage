6dbff259f714e6c1edf9ef684db04489fa3614a7
marksaunders341 <103631116+marksaunders341@users.noreply.github.com>
2022-10-27 09:48:06 +0100

    Organization members table role filters added

--- src/pages/dashboard/organization/OrganizationMembersTable.tsx
+++ src/pages/dashboard/organization/OrganizationMembersTable.tsx
@@ -5,13 +5,16 @@ import {
     GridColDef,
     GridRenderCellParams,
     GridToolbar,
-    GridValueGetterParams
+    GridValueGetterParams,
+    GridFilterOperator,
+    GridFilterItem,
+    GridFilterInputValueProps
 } from "@mui/x-data-grid";
 import OrganizationMoreMenu from "../user/OrganizationMoreMenu";
 import {useEffect, useState} from "react";
 import {Organization, User, UserInOrganization, UserRole} from "../../../API";
 import {API, graphqlOperation} from "aws-amplify";
-import {Button, IconButton, Menu, MenuItem, Stack, Tooltip} from "@mui/material";
+import {Button, IconButton, Menu, MenuItem, Stack, Tooltip, Box, TextField, Select, FormControl, InputLabel} from "@mui/material";
 import Iconify from "../../../components/Iconify";
 import {useParams} from "react-router-dom";
 import MemberRolesMenu from "./MemberRolesMenu";
@@ -42,7 +45,8 @@ const query = `query MyQuery($id: ID = "") {
     }
   }
 }
-`
+`;
+
 const getRolesQuery = `query MyQuery($id: ID = "") {
   getOrganization(id: $id) {
     roles {
@@ -62,11 +66,10 @@ export default function OrganizationMembersTable() {
     const {user} = useAuth();
     const {organizationId} = useParams();
     const [roles, setRoles] = useState<UserRole[] | null>(null);
-    const MemberTableItem = (params: GridRenderCellParams) => {
 
+    const MemberTableItem = (params: GridRenderCellParams) => {
         const roles: any = params.getValue(params.id, 'roles');
         const allRoles: any = params.getValue(params.id, 'allRoles');
-
         return (
             <Stack direction={'row'} spacing={2}>
                 <MemberRolesMenu id={String(params.id)} roles={roles} allRoles={allRoles}
@@ -75,12 +78,68 @@ export default function OrganizationMembersTable() {
             </Stack>
         );
     }
+
+    function RoleInputValue(props: GridFilterInputValueProps) {
+
+        const { item, applyValue } = props;
+
+        const handleFilterChange = (event: any) => {
+            applyValue({ ...item, value: event.target.value});
+        };
+
+        return (
+            <TextField label="Filter value"
+                       variant="standard"
+                       onChange={(event) => handleFilterChange(event)} />
+        );
+
+    }
+
+    const rolesOnlyOperators: GridFilterOperator[] = [
+        {
+            label: 'starts with',
+            value: 'startsWith',
+            getApplyFilterFn: (filterItem: GridFilterItem, column: GridColDef) => {
+                if (!filterItem.columnField || !filterItem.value || !filterItem.operatorValue) {
+                    return null;
+                }
+                return (params: GridCellParams): boolean => {
+                    let filter = filterItem.value.toLowerCase();
+                    for (let role of params.value) {
+                        if (role.userRole.name.toLowerCase().startsWith(filter)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                };
+            },
+            InputComponent: RoleInputValue,
+            InputComponentProps: { type: 'string' }
+        },
+        {
+            label: 'is empty',
+            value: 'isEmpty',
+            getApplyFilterFn: (filterItem: GridFilterItem, column: GridColDef) => {
+                if (!filterItem.columnField || !filterItem.operatorValue) {
+                    return null;
+                }
+                return (params: GridCellParams): boolean => {
+                    if (params.value.length == 0) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                };
+            }
+        }
+    ];
+
     const columns: GridColDef[] = [
         {field: 'id', flex: 0.2, headerName: 'Id', hide: true},
         {
             field: 'email', flex: 1, headerName: 'Email',
             minWidth: 100,
-
         },
         {
             field: 'firstName',
@@ -109,17 +168,18 @@ export default function OrganizationMembersTable() {
         },
         {
             field: 'roles',
-            headerName: 'Actions',
+            headerName: 'Roles',
             description: 'This column has a value getter and is not sortable.',
             sortable: false,
             flex: 0.6,
             align: 'center',
             headerAlign: 'center',
             renderCell: MemberTableItem,
+            filterOperators: rolesOnlyOperators
         },
         {
             field: 'allRoles',
-            headerName: 'Roles',
+            headerName: 'All Roles',
             description: 'This column has a value getter and is not sortable.',
             sortable: false,
             flex: 0.4,
@@ -127,9 +187,8 @@ export default function OrganizationMembersTable() {
             headerAlign: 'center',
             hide: true
         },
-
-
     ];
+
     const [members, setMembers] = useState<UserInOrganization[] | null>(null);
 
     async function getOrganizations() {
@@ -143,16 +202,12 @@ export default function OrganizationMembersTable() {
         setRoles(result.data.getOrganization.roles.items);
     }
 
-
     useEffect(() => {
-
         getRolesAsync();
         getOrganizations();
         return () => {
-
         };
     }, []);
-
 
     return (
         <div>
@@ -181,6 +236,6 @@ export default function OrganizationMembersTable() {
                 <Iconify icon={'mdi:cached'} sx={{height: 30, width: 30}}/>
             </IconButton>
         </div>
-
     );
+
 }
